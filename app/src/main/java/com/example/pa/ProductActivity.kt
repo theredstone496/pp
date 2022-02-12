6d466e0176e0dd0a67213d43c9c46af501f35ebd
theredstone496 <theredstone496@gmail.com>
2022-02-12 15:02:45 +0800

    Added second activity for product details

---
+++ app/src/main/java/com/example/pa/ProductActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pa
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+//to view more product details
+class ProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product)
+    }
+}
